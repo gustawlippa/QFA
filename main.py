75031d9c8cdb496372a8f6798bdd64fb77e25013
kmakiela <makiela11@gmail.com>
2019-11-25 15:27:18 +0100

    a little reorganisation

---
+++ main.py
@@ -0,0 +1,14 @@
+import PFA as PFA
+import MO_1QFA as MO
+import MM_1QFA as MM
+
+
+def main():
+    PFA.dfa_example()
+    PFA.pfa_example()
+    MO.mo_1qfa_example()
+    MM.mm_1qfa_example()
+
+
+if __name__ == "__main__":
+    main()
