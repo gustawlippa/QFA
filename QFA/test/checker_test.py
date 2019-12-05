59a0750e3757a0e330d633c9d4d074697b4bc37a
kmakiela <makiela11@gmail.com>
2019-12-05 14:23:17 +0100

    update LanguageChecker and its tests

--- QFA/test/checker_test.py
+++ QFA/test/checker_test.py
@@ -8,6 +8,28 @@ class LanguageCheckerTest(unittest.TestCase):
         gqfa_checker = Checker.LanguageChecker(gqfa, ["aa", "aaa"], ["a"])
 
         self.assertEqual(gqfa_checker.automata, gqfa)
+        self.assertEqual(gqfa_checker.language, ["aa", "aaa"])
+        self.assertEqual(gqfa_checker.not_in_language, ["a"])
+
+    def test_gqfa_check(self):
+        gqfa = GQFA.example()
+        gqfa_checker = Checker.LanguageChecker(gqfa, ["aa", "aaa"], ["a"])
+        gqfa_checker.check_language()
+
+        self.assertIn('cutpoint', gqfa_checker.accepted)
+        self.assertIn('isolated_cutpoint', gqfa_checker.accepted)
+        self.assertIn('positive_unbounded', gqfa_checker.accepted)
+        self.assertNotIn('Monte_Carlo', gqfa_checker.accepted)
+        self.assertNotIn('bounded', gqfa_checker.accepted)
+        self.assertNotIn('negative_unbounded', gqfa_checker.accepted)
+
+        lambda_, epsilon, error = gqfa_checker.accepted['isolated_cutpoint']
+
+        self.assertAlmostEqual(lambda_, 0.609375, delta=error)
+        self.assertAlmostEqual(epsilon, 0.109375, delta=error)
+        self.assertLess(error, 10**(-15))
+
+        self.assertAlmostEqual(lambda_ + epsilon, gqfa_checker.accepted['cutpoint'], delta=error)
 
 
 if __name__ == '__main__':
